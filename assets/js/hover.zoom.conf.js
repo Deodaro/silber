4d94eda4c01af92857c6868babb2375194a855b4
Deodaro <l_r@list.ru>
2021-02-07 15:57:02 +1000

    Change zoom hover color

--- assets/js/hover.zoom.conf.js
+++ assets/js/hover.zoom.conf.js
@@ -1,32 +1,11 @@
-        $(function() {
-        
-        	
-            $('.blue').hoverZoom({
-                overlayColor: '#3498db',
-                zoom: 0
-            });
-            
-            $('.green').hoverZoom({
-                overlayColor: '#1abc9c',
-                zoom: 0
-            });
-            
-            $('.pink').hoverZoom({
-                overlayColor: '#bd2e75',
-                zoom: 0
-            });
-            
-            $('.black').hoverZoom({
-                overlayColor: '#2f2f2f',
-                zoom: 0
-            });
-            
-            $('.alizarin').hoverZoom({
-                overlayColor: '#e74c3c',
-                zoom: 0
-            });
-            
-            /* USAGE
+$(function () {
+  $('.colored').hoverZoom({
+    // overlayColor: '#4b6a78',
+    overlayColor: 'rgba(75, 106, 120, .7)',
+    zoom: 0,
+  });
+
+  /* USAGE
             
             $('#pink').hoverZoom({
                 overlay: true, // false to turn off (default true)
@@ -37,5 +16,4 @@
             });
             
             */
-            
-        }); 
+});
